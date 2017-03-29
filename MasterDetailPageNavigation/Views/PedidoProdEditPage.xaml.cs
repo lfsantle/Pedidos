5145754c3c129b55f9232250f94860a16eecf72f
Liv3n <contato@liv3n.com.br>
2017-03-28 21:37:27 -0300

    Item 1 e 2

--- MasterDetailPageNavigation/Views/PedidoProdEditPage.xaml.cs
+++ MasterDetailPageNavigation/Views/PedidoProdEditPage.xaml.cs
@@ -9,47 +9,50 @@ namespace MasterDetailPageNavigation
 	public partial class PedidoProdEditPage : ContentPage
 	{
 		public string PRECO1;
+		public int qtde = 0;
+		double subtotal = 0;
+		double valor = 0;
 
 		public PedidoProdEditPage(Produtos PRODSelecionado)
 		{
 			InitializeComponent();
-			this.Title = "Add Produto";
+			this.Title = "Adicionar Produto";
 
+			valor = Convert.ToDouble(PRODSelecionado.PRECO1);
 			Lbl_PRODDESC.Text = PRODSelecionado.DESCPRO;
 			Lbl_UNIT.Text = "R$ " + PRODSelecionado.PRECO1.Replace('.', ',');
 			PRECO1 = PRODSelecionado.PRECO1;
 			Lbl_CodBarra.Text = PRODSelecionado.CODBAR;
 			Lbl_CODPROD.Text = PRODSelecionado.CODIGO;
-			Qtde.Text = "1";
+			//Qtde.Text = "1";
 			Calcular();
 			Qtde.TextChanged += Qtde_TextChanged;
 		}
 
+
 		private void Calcular()
 		{
-			double subtotal = 0;
-			double valor = 0;
-			int qtde = 1;
-			try
-			{
-				valor = Convert.ToDouble(Lbl_UNIT.Text.Replace("R$ ","").Replace('.', ','));
-			}
-			catch (Exception ex)
-			{
-				//DisplayAlert("Erro ao Calcular Unit", ex.Message, "OK");
-			}
-			try
-			{
-				qtde = Convert.ToInt32(Qtde.Text);
-			}
-			catch (Exception ex)
-			{
-				//DisplayAlert("Erro ao Calcular Qtde", ex.Message, "OK");
-			}
 
-			subtotal = valor * qtde;
-			Subtotal.Text = "R$ " + subtotal.ToString();
-			Qtde.Text = qtde.ToString();
+			if (!string.IsNullOrEmpty(Qtde.Text))
+			{
+				try
+				{
+					qtde = Convert.ToInt32(Qtde.Text);
+				}
+				catch (Exception ex)
+				{
+					DisplayAlert("Erro ao Calcular Qtde", ex.Message, "OK");
+				}
+
+				subtotal = valor * qtde;
+				Subtotal.Text = "R$ " + subtotal.ToString();
+				Qtde.Text = qtde.ToString();
+			}
+			else
+			{
+				subtotal = 0;
+				Subtotal.Text = "";
+			}
 		}
 
 		private void Qtde_TextChanged(object sender, TextChangedEventArgs e)
@@ -59,23 +62,30 @@ namespace MasterDetailPageNavigation
 
 		private async void GravarProduto(object sender, EventArgs e)
 		{
-
-			var answer = await DisplayAlert("Confirmação", "Deseja adicionar o produto?", "Sim", "Não");
-			if (answer)
+			if (string.IsNullOrEmpty( Qtde.Text )) 
 			{
-				string detalhe = "R$ " + PRECO1 + " * " + Qtde.Text + " = " + Subtotal.Text;
-				PedidosClass._PedidoItem.Add(new PedidoItem
+				await DisplayAlert("Atenção", "Insira a quantidade desejada!", "OK");
+			}
+			else
+			{
+
+				var answer = await DisplayAlert("Confirmação", "Deseja adicionar o produto?", "Sim", "Não");
+				if (answer)
 				{
-					SEQ = "",
-					CUPOM = "",
-					CODPROD = Lbl_CODPROD.Text,
-					DESCRICAO = Lbl_PRODDESC.Text,
-					UNIT = PRECO1,
-					QUANT = Qtde.Text,
-					TOTAL = Subtotal.Text.Replace("R$ ", "").Replace(',', '.'),
-					DETALHE = detalhe
-				});
-				await Navigation.PushAsync(new PedidosCadPage("adicionado"));
+					string detalhe = "R$ " + PRECO1 + " * " + Qtde.Text + " = " + Subtotal.Text;
+					PedidosClass._PedidoItem.Add(new PedidoItem
+					{
+						SEQ = "",
+						CUPOM = "",
+						CODPROD = Lbl_CODPROD.Text,
+						DESCRICAO = Lbl_PRODDESC.Text,
+						UNIT = PRECO1,
+						QUANT = Qtde.Text,
+						TOTAL = Subtotal.Text.Replace("R$ ", "").Replace(',', '.'),
+						DETALHE = detalhe
+					});
+					await Navigation.PushAsync(new PedidosCadPage("adicionado"));
+				}
 			}
 
 		}
